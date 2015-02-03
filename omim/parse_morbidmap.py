c7adc5815340bd6cb51599a5143cf71a0e242d5b
toby <tong.shu.li@gmail.com>
2015-02-02 16:11:21 -0800

    parsing morbidmap now includes the name of the dmim

--- omim/parse_morbidmap.py
+++ omim/parse_morbidmap.py
@@ -1,4 +1,4 @@
-# last updated 2015-01-26 toby
+# last updated 2015-02-02 toby
 import re
 import sys
 sys.path.append("/home/toby/global_util/")
@@ -7,21 +7,34 @@ from file_util import read_file
 
 from collections import defaultdict
 
+def info(s):
+	result = re.search(r'\d{6}', s)
+	if result is not None:
+		dmim = result.group()
+		name = s.rsplit(",", 1)[0]
+		return (dmim, name)
+
+	return ()
+
 def parse_morbidmap():
 #	returns genes[dmim] = set(gmim, gmim, gmim...) to ensure uniqueness
 #	gives the list of gene mim numbers known to cause each disease mim number
 
 	genes = defaultdict(set) # all unique gmims assosicated with dmim
+	name = dict() # name of dmim
 
 	mm_loc = "/home/toby/databases/omim/data/"
 	for line in read_file("morbidmap.txt", mm_loc):
 		disease, gene, gmim, locus = line.split("|")
-		result = re.search(r'\d{6}', disease)
 
-#		some diseases are bad and have no disease mim identifier
-		if result is not None:
-			dmim = result.group()
-			genes[dmim].add(gmim)
+		res = info(disease)
+		if res:
+			genes[res[0]].add(gmim)
+			name[res[0]] = res[1]
+
+	with open("dmim_name.txt", "w") as out:
+		for dmim, disease_name in name.items():
+			out.write("{0}|{1}\n".format(dmim, disease_name))
 
 	return genes
 
