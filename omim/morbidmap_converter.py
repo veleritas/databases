40d018bafa1e33d989899235aabc1d90d672acce
toby <tong.shu.li@gmail.com>
2015-03-02 20:29:15 -0800

    dumb way of printing things

--- omim/morbidmap_converter.py
+++ omim/morbidmap_converter.py
@@ -1,4 +1,47 @@
-# last updated 2015-01-27 toby
+# last updated 2015-03-02 toby
+"""
+Converts OMIM gene and disease identifiers into Entrez gene IDS
+and UMLS CUIs.
+"""
+from parse_morbidmap import parse_morbidmap
+
+def main():
+	bad_dmims = set()
+	genes = parse_morbidmap()
+	with open("./data/converted_morbidmap2.txt", "w") as out:
+		for dmim, gmims in sorted(genes.items()):
+			cuis = convert.dmim_to_cui(dmim)
+			if not cuis:
+				bad_dmims.add(dmim)
+
+
+
+
+			gene_ids = set()
+			for gmim in gmims:
+				gene_id = convert.gmim_to_geneID(gmim)
+				if temp:
+					gene_ids.add(gene_id)
+
+			out.write("{0}|{1}\n".format(dmim, "|".join(cuis)))
+			out.write("\t{0}\n".format("|".join(gene_ids)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 from parse_morbidmap import parse_morbidmap
 
@@ -8,56 +51,46 @@ import convert
 from file_util import read_file
 
 def main():
-	genes = parse_morbidmap()
-	dmims = sorted(list(genes))
+#	mm_loc = "/home/toby/databases/omim/data/"
 
-	N = len(dmims)
+	bad_dmims = set()
+	with open("./data/converted_morbidmap.txt", "w") as out:
+		for line in read_file("./data/sorted_morbidmap.txt"):
+			line = line.lstrip('#')
+			vals = line.split('|')
 
-	bad_dmims = []
-	bad_gmims = []
-	outloc = "/home/toby/databases/omim/"
-	with open(outloc + "converted_morbidmap.txt", "w") as out:
-		for i, dmim in enumerate(dmims):
-			print "{0}/{1}, {2}".format(i+1, N, dmim)
+			dmim = vals[0]
+			gmims = vals[1:]
 
 			cuis = convert.dmim_to_cui(dmim)
 			if not cuis:
-				bad_dmims.append(dmim)
+				bad_dmims.add(dmim)
 
-			print "cuis", cuis
+			gene_ids = set()
+			for gmim in gmims:
+				temp = convert.gmim_to_geneID(gmim)
+				if temp:
+					gene_ids.add(temp)
 
-			gmims = genes[dmim]
-			temp_ids = map(convert.gmim_to_geneID, gmims)
+			print "dmim", dmim
 
-			print "gene ids", temp_ids
-
-			gene_ids = []
-			for sublist, gmim in zip(temp_ids, gmims):
-				gene_ids += sublist
-				if len(sublist) == 0 or len(sublist) > 1:
-					bad_gmims.append((gmim, sublist))
-
-
+#			print to file
 			out.write(dmim)
 			for cui in cuis:
 				out.write("|" + cui)
 			out.write('\n')
 
-			out.write("\t")
+			out.write('\t')
 			for j, gid in enumerate(gene_ids):
 				out.write(gid)
 				if j < len(gene_ids) - 1:
-					out.write("|")
-			out.write("\n")
+					out.write('|')
+			out.write('\n')
 
-	with open(outloc + "bad_dmims.txt", "w") as out:
-		for dmim in bad_dmims:
-			out.write(dmim + "\n")
-
-	with open(outloc + "bad_gmims.txt", "w") as out:
-		for gmim, sublist in bad_gmims:
-			out.write("gmim " + gmim + "\n")
-			out.write(str(sublist) + "\n")
+	with open("bad_dmims.txt", "w") as out:
+		for val in bad_dmims:
+			out.write(val + "\n")
 
 if __name__ == "__main__":
 	main()
+	print "done"
