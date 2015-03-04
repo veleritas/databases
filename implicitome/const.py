42b50f7746981fda227d2376ee40382c53abc029
toby <tong.shu.li@gmail.com>
2015-03-03 16:27:59 -0800

    accidental upload of database access credentials

--- implicitome/const.py
+++
@@ -1,9 +0,0 @@
-# last updated 2015-01-23 toby
-# do not upload to the internet
-
-DB_INFO = {
-	"user": "toby",
-	"password": "remember_the_mice!",
-	"host": "localhost",
-	"database": "implicitome"
-}
