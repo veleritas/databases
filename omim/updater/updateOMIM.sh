f3760f0db9f51a485a6f43497ae262598a548928
toby <tong.shu.li@gmail.com>
2015-05-06 10:58:24 -0700

    replaced tabs with spaces

--- omim/updater/updateOMIM.sh
+++ omim/updater/updateOMIM.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-# last updated 2015-01-26 toby
+# last updated 2015-05-06 toby
 
-files=('morbidmap')
+files=('morbidmap' 'genemap.key')
 
 echo "Updating: ${files[@]}"
 
 ./ftpOMIM.exp ${files[@]}
 
 for fname in "${files[@]}"; do
-	newname=$fname
-	if [[ $fname != *".txt" ]]; then
-		newname+=".txt"
-	fi
+    newname=$fname
+    if [[ $fname != *".txt" ]]; then
+        newname+=".txt"
+    fi
 
-	if [ -e "$fname" ]; then
-		echo "Successfully got $fname"
-		mv $fname ~/databases/omim/data/$newname
-	else
-		echo "Could not get $fname from OMIM server."
-	fi
+    if [ -e "$fname" ]; then
+        echo "Successfully got $fname"
+        mv $fname ../data/$newname
+    else
+        echo "Could not get $fname from OMIM server."
+    fi
 done
 
 echo "Done."
